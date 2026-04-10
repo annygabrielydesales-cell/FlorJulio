5898f062ccfb6f434e812fe08c3ed2d645f38a6c
annygabrielydesales-cell <annygabrielydesales@gmail.com>
2026-04-10 01:06:52 +0100

    Update main.js

--- main.js
+++ main.js
@@ -3,3 +3,9 @@
 window.addEventListener("load", () => {
   document.body.classList.remove("not-loaded");
 });
+onload = () => {
+    const c = setTimeout(() => {
+      document.body.classList.remove("not-loaded");
+      clearTimeout(c);
+    }, 1000);
+  };
